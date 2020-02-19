af4f7a698d84569f9552a15d6a0910c41dac0ee1
otdavies <oliver@psyfer.io>
2020-02-19 00:36:28 -0800

    Added portal effect!

---
+++ Assets/WinterFresh/Scripts/Rendering/RenderQueue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderQueue : MonoBehaviour
+{
+    public int queue;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GetComponent<MeshRenderer>().material.renderQueue = queue;
+    }
+}
